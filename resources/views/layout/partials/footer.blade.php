844c587173a6db48f88a0980fad8841bc8e6a631
Klvst3r <klvst3r@gmail.com>
2025-07-01 09:49:56 -0600

    Corrección en integración de los JS

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -13,18 +13,16 @@
       </div>
     </div>
 
+    <!-- jQuery -->
+    <script src="{{ asset('dashboard/vendors/jquery/dist/jquery.min.js') }}"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script> --}}
 
     <!-- Fullscreen -->
-
     <script src="{{ asset('js/fullscreen.js') }}"></script>
 
-
-    <!-- jQuery -->
-    {{-- <script src="{{ asset('dashboard/vendors/jquery/dist/jquery.min.js') }}"></script> --}}
-        
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <!-- Bootstrap -->
     <script src="{{ asset('dashboard/vendors/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+
     <!-- FastClick -->
     <script src="{{ asset('dashboard/vendors/fastclick/lib/fastclick.js') }}"></script>
     <!-- NProgress -->
@@ -39,27 +37,29 @@
     <script src="{{ asset('dashboard/vendors/iCheck/icheck.min.js') }}"></script>
     <!-- Skycons -->
     <script src="{{ asset('dashboard/vendors/skycons/skycons.js') }}"></script>
+
     <!-- Flot -->
     <script src="{{ asset('dashboard/vendors/Flot/jquery.flot.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/Flot/jquery.flot.pie.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/Flot/jquery.flot.time.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/Flot/jquery.flot.stack.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/Flot/jquery.flot.resize.js') }}"></script>
+
     <!-- Flot plugins -->
     <script src="{{ asset('dashboard/vendors/flot.orderbars/js/jquery.flot.orderBars.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/flot-spline/js/jquery.flot.spline.min.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/flot.curvedlines/curvedLines.js') }}"></script>
+
     <!-- DateJS -->
     <script src="{{ asset('dashboard/vendors/DateJS/build/date.js') }}"></script>
     <!-- JQVMap -->
     <script src="{{ asset('dashboard/vendors/jqvmap/dist/jquery.vmap.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js') }}"></script>
+
     <!-- bootstrap-daterangepicker -->
     <script src="{{ asset('dashboard/vendors/moment/min/moment.min.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/bootstrap-daterangepicker/daterangepicker.js') }}"></script>
-
-   
 
     <!-- DarkMode -->
     <script src="{{ asset('js/dark-mode.js') }}"></script>
@@ -80,8 +80,6 @@
     <script src="{{ asset('dashboard/vendors/jszip/dist/jszip.min.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/pdfmake/build/pdfmake.min.js') }}"></script>
     <script src="{{ asset('dashboard/vendors/pdfmake/build/vfs_fonts.js') }}"></script>
-  
-
   
     <!-- Parsley principal -->
     {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.3/parsley.min.js"></script> --}}
